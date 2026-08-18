d49cda5bf0e0aa0dbed2e6be3c02e09f037d83f4
Tamerlan Ordukhanov <tamerlan.ord2005@gmail.com>
2026-08-19 00:23:46 +0300

    feat: add Comment model with CRUD operations and visibility logic

---
+++ models/comment.py
@@ -0,0 +1,69 @@
+from datetime import datetime, UTC
+from database.db import db
+from utils.permissions import can_view_comment, can_edit_comment, can_delete_comment
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+    is_hidden = db.Column(db.Boolean, default=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(UTC))
+    
+    author = db.relationship('User', backref=db.backref('comments', lazy='dynamic'))
+    post = db.relationship('Post', backref=db.backref('comments', lazy='dynamic'))
+    likes = db.relationship('CommentLike', backref='comment', lazy='dynamic')
+
+
+def get_comments_for_post(post, user=None, sort=1):
+    sort = int(sort) if sort else 1
+    comments = db.session.query(Comment).filter_by(post_id=post.id).all()
+    comments = [comment for comment in comments if can_view_comment(user, post, comment)]
+    if sort == 1:
+        comments.sort(key=lambda c: c.created_at, reverse=True)
+    else:
+        comments.sort(key=lambda c: c.likes.count(), reverse=True)
+
+    return comments
+
+def get_comment(comment_id):
+    comment = db.session.query(Comment).filter_by(id=comment_id).first()
+    return comment
+
+def add_comment(post_id, user_id, content):
+    comment = Comment(author_id=user_id, post_id=post_id, content=content)
+    
+    db.session.add(comment)
+    db.session.commit()
+
+def edit_comment(comment_id, content):
+    comment = db.session.get(Comment, comment_id)
+    
+    if comment:
+        comment.content = content
+        db.session.commit()
+        return True
+    
+    return False
+
+def delete_comment(comment_id):
+    comment = db.session.get(Comment, comment_id)
+        
+    if comment:
+        db.session.delete(comment)
+        db.session.commit()
+        return True
+
+    return False
+
+def hide_comment(comment_id):
+    comment = db.session.get(Comment, comment_id)
+        
+    if comment:
+        comment.is_hidden = 0 if comment.is_hidden else 1
+        db.session.commit()
+        return True
+
+    return False
--- utils/permissions.py
+++ utils/permissions.py
@@ -1,3 +1,4 @@
+#Posts
 def can_view_post(user, post):
     if post.is_public:
         return True
@@ -14,3 +15,19 @@ def can_delete_post(user, post):
     if not user:
         return False
     return user.id == post.author_id or user.role in ("moderator", "admin")
+
+#Comments
+def can_view_comment(user, post, comment):
+    if not comment.is_hidden:
+        return True
+    return user.id == post.author_id or user.role in ("moderator", "admin")
+
+def can_edit_comment(user, comment):
+    if not user:
+        return False
+    return user.id == comment.author_id or user.role == "admin"
+
+def can_delete_comment(user, comment):
+    if not user:
+        return False
+    return user.id == comment.author_id or user.role in ("moderator", "admin")
