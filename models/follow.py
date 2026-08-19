8bdb23a5dcc4ec8f6040af687e433714ab1ffae7
Tamerlan Ordukhanov <tamerlan.ord2005@gmail.com>
2026-08-19 16:58:44 +0300

    feat: add Follow model for user subscriptions

---
+++ models/follow.py
@@ -0,0 +1,40 @@
+from datetime import datetime, UTC
+from database.db import db
+
+class Follow(db.Model):
+    __tablename__ = "follows"
+
+    id = db.Column(db.Integer, primary_key=True)
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    following_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(UTC))
+    
+    follower = db.relationship('User', foreign_keys=[follower_id], backref=db.backref('following', lazy='dynamic'))
+    following = db.relationship('User', foreign_keys=[following_id], backref=db.backref('followers', lazy='dynamic'))
+    following = db.relationship('User', backref=db.backref('follows', lazy='dynamic'))
+    
+    __table_args__ = (db.UniqueConstraint('follower_id', 'following_id', name='unique_'),)
+
+#Функции: follow_user, unfollow_user, is_following, get_followers, get_following
+
+def follow_user(follower_id, following_id):
+    if follower_id != following_id:
+        follow = Follow(follower_id=follower_id, following_id=following_id)
+        db.session.add(follow)
+        db.session.commit()
+
+def unfollow_user(follower_id, following_id):
+    follow = db.session.query(Follow).filter_by(follower_id=follower_id, following_id=following_id).first()
+    if follow:
+        db.session.delete(follow)
+        db.session.commit()
+
+def is_following(follower_id, following_id):
+    follow = db.session.query(Follow).filter_by(follower_id=follower_id, following_id=following_id).first()
+    return follow is not None
+
+def get_followers(following_id):
+    return db.session.query(Follow).filter_by(following_id=following_id).all()
+
+def get_following(follower_id):
+    return db.session.query(Follow).filter_by(follower_id=follower_id).all()
